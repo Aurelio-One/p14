21ae96b246c05327ff5786a069026cc2470f6406
Aurelio <maurici.aurelio@gmail.com>
2023-12-19 13:45:56 +0100

    [Add]Add useFormData custom hook

---
+++ src/hooks/useFormData.jsx
@@ -0,0 +1,80 @@
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  setData,
+  setDataEmployee,
+  setIsSubmitted,
+} from '../features/form/formSlice'
+import { setErrorsMsg } from '../features/error/errorSlice'
+import useErrorMsg from './useErrorMsg'
+
+const useFormData = () => {
+  const dispatch = useDispatch()
+  const { data, dataEmployee } = useSelector((state) => state.form)
+  const { validateField } = useErrorMsg()
+
+  const initialData = {
+    firstname: '',
+    lastname: '',
+    dateOfBirth: '',
+    startDate: '',
+    street: '',
+    city: '',
+    state: '',
+    code: '',
+    department: '',
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    dispatch(setData({ ...data, [name]: value }))
+    validateField(name, value)
+  }
+
+  const resetForm = () => {
+    dispatch(setData(initialData))
+    dispatch(setErrorsMsg({}))
+  }
+
+  const saveEmployee = (e) => {
+    e.preventDefault()
+
+    const requiredFields = [
+      'firstname',
+      'lastname',
+      'dateOfBirth',
+      'startDate',
+      'street',
+      'city',
+      'state',
+      'code',
+      'department',
+    ]
+
+    let newErrors = {}
+    let hasErrors = false
+    requiredFields.forEach((field) => {
+      if (!data[field]) {
+        newErrors[field] = `Please enter your ${field}`
+        hasErrors = true
+      }
+    })
+
+    dispatch(setErrorsMsg(newErrors))
+
+    if (!hasErrors) {
+      dispatch(setDataEmployee([...dataEmployee, data]))
+      dispatch(setIsSubmitted(true))
+      resetForm()
+    } else {
+      dispatch(setIsSubmitted(false))
+    }
+  }
+
+  return {
+    handleChange,
+    resetForm,
+    saveEmployee,
+  }
+}
+
+export default useFormData
